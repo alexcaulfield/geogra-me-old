13d65ac0b08ded62afee376b1f84a478cd5d902c
Alex Caulfield <alex.caulfield28@gmail.com>
2020-05-26 00:02:41 -0400

    updated url

--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,4 @@
-export const SITE_URL = 'https://geogra.me/'
+export const SITE_URL = 'https://geogra.me';
 
 export const USERS_COLLECTION = 'users';
 export const GOOGLE_MAP_URL = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyAq-bT8CcgFTuTQZjkjLCRFSawl4k9jJ_Q&libraries=geometry,drawing,places';

69de361841abc258f72e247c20ada34bdb86f30e
Alex Caulfield <alex.caulfield28@gmail.com>
2020-05-25 23:15:20 -0400

    bug with url generation

--- src/utils.js
+++ src/utils.js
@@ -1,8 +1,4 @@
-import process from "process";
-
-const isDev = () => !process.env.NODE_ENV || process.env.NODE_ENV === 'development';
-
-export const SITE_URL = isDev() ? 'localhost:3000' : 'https://geogra.me/';
+export const SITE_URL = 'https://geogra.me/'
 
 export const USERS_COLLECTION = 'users';
 export const GOOGLE_MAP_URL = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyAq-bT8CcgFTuTQZjkjLCRFSawl4k9jJ_Q&libraries=geometry,drawing,places';
